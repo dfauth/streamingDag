53c65a97a42f5f540b527aa59acd84a50fb3445b
damian <dfauth@gmail.com>
2022-06-15 07:58:43 +0800

    refactoring

--- src/main/java/com/github/dfauth/stream/dag/SubscriberFunction.java
+++ src/main/java/com/github/dfauth/stream/dag/SubscriberFunction.java
@@ -6,9 +6,9 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- * A Subscriber<T> whose payload is a Function<T,R> and which also implements Function<T,Optional<R>>
- * This is used to implement a Function applied to a stream whose implementation can change - by partially applying streamed values (from another stream)
- * to a function
+ * A Consumer<T> who accepts a Function<T,R> and which also implements Function<T,Optional<R>>
+ * as long as the consumer is not called, the function operation will always return Optional.empty()
+ * once a function is defined, it will be applied to the input when apply is called abd returned wrapped in an Optional
  */
 public class SubscriberFunction<T,R> implements Consumer<Function<T,R>>, Function<T,Optional<R>> {
 
